6fb1169d94c9acf3c67c2d26c88e4ccc8d0a68bc
David Siebert <david.siebert@zeiss.com>
2021-07-09 09:04:24 +0200

    Add Backend Projekt

---
+++ backend/WebApi/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hallo David v1 - Run on OpenShift ;)";
+        }
+    }
+}
